2224be488dc2b89729639c4b5cb472ee62dda658
Clifford Morin <cemorin21@gmail.com>
2023-04-16 14:56:50 -0700

    more changes

--- src/components/Calendar/CalendarV2.js
+++ src/components/Calendar/CalendarV2.js
@@ -15,60 +15,6 @@ import EventCard from "./EventCard";
 import EmptyCalendar from "./EmptyCalendar";
 import LoadingPage from "./LoadingPage";
 
-// let ids = Array.from({ length: 15 }, (_, i) => i + 1);
-// let citys = Array.from({ length: 15 }, (_, i) => "Amsterdam, NL");
-// let band = [
-//   "Philip Lassiter",
-//   "Philip Lassiter",
-//   "Ton Felices Quartet",
-//   "NJJO last concert",
-//   "The Dam Jawn feat. Dick Oatts",
-//   "The Dam Jawn feat. Dick Oatts",
-//   "The Dam Jawn feat. Dick Oatts",
-//   "The Dam Jawn feat. Dick Oatts",
-//   "The Dam Jawn feat. Dick Oatts",
-//   "The Dam Jawn feat. Dick Oatts",
-//   "The Dam Jawn feat. Dick Oatts",
-//   "Ernesto Montenegro & Martin Diaz quintet",
-//   "Oscar Ployart and friends",
-//   "Martin Diaz plays Charlie Parker with Strings feat. Arts Simfònica Jove",
-//   "Martin Diaz plays Charlie Parker with Strings feat. Arts Simfònica Jove",
-// ];
-// let date = [
-//   "FEB 25",
-//   "MAR 23",
-//   "MAR 24",
-//   "APR 17",
-//   "MAY 13",
-//   "MAY 14",
-//   "MAY 17",
-//   "MAY 18",
-//   "MAY 19",
-//   "MAY 20",
-//   "MAY 21",
-//   "JUN 15",
-//   "JUL 27",
-//   "JUL 29",
-//   "AUG 4",
-// ];
-// let location = [
-//   "S’Hertogenbosch (Live Soul Festival)",
-//   "Germany",
-//   "Jazz Cafe Alto (Amsterdam)",
-//   "Cinetol (Amsterdam)",
-//   "Venue (Rotterdam) ",
-//   "Fizi Theatre (Zierikzee)",
-//   "ZonaJazz (Torre de Fontaubella)",
-//   "Auditori Caló del Soli (Sant Josep de Sa Talaia, Ibiza)",
-//   "Jamboree (Barcelona)",
-//   "La Mar de Jazz (Cambrils)",
-//   "Ultramar (L’Escala)",
-//   "The WolfHound (Haarlem)",
-//   "Robadors23 (Barcelona)",
-//   "Jamboree (Barcelona)",
-//   "Nits de Jazz (Platja d’Aro)",
-// ];
-
 const API_KEY = process.env.REACT_APP_BANDSINTOWN_API_KEY;
 const ARTIST_ID = "12667113";
 
@@ -86,7 +32,6 @@ const CalendarV2 = () => {
       setLoading(true);
       const response = await fetch(API_URL);
       const data = await response.json();
-      console.log(data);
       setEvents(data);
     } catch (error) {
       setError(error);
@@ -102,16 +47,6 @@ const CalendarV2 = () => {
   useEffect(() => {
     fetchEvents();
   }, [fetchEvents]);
-
-  // let events2 = citys.map((city, i) => {
-  //   return {
-  //     id: ids[i],
-  //     city,
-  //     band: band[i],
-  //     date: date[i],
-  //     location: location[i],
-  //   };
-  // });
 
   return (
     <div>
